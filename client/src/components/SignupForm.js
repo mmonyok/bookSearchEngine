a874fe632c4b89db99b73db875e5af9eae1c48c5
Ody <melodymonyok@gmail.com>
2021-09-20 11:40:19 -0500

    Adjusted form to use mutations.

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [addUser] = useMutation(ADD_USER);
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
@@ -28,14 +29,14 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      const { data } = await addUser({
+        variables: { userFormData },
+      });
+      const { token, user } = data.addUser;
+      if (!token || !user ) {
+        throw new Error("There was something wrong with your login!");
       }
 
-      const { token, user } = await response.json();
-      console.log(user);
       Auth.login(token);
     } catch (err) {
       console.error(err);
