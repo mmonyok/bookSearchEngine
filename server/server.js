76f4be7011e0c134a82464e7879f60c4ff3c7c11
Ody <melodymonyok@gmail.com>
2021-09-22 18:43:27 -0500

    Commented out some things for development.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const { ApolloServer } = require ('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const { typeDefs, resolvers } = require ('./schemas');
+
+const { typeDefs, resolvers } = require('./schemas');
 const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -11,14 +13,23 @@ let server;
 
 async function startServer() {
   server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: authMiddleware
-});
+    typeDefs,
+    resolvers,
+    context: authMiddleware
+  });
   await server.start();
   server.applyMiddleware({ app });
 };
 startServer();
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+//   context: authMiddleware,
+// });
+
+// server.applyMiddleware({ app });
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -27,9 +38,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
