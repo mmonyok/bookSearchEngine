2c9436071c375969a685d5f1db9cae7ff54640ce
Ody <melodymonyok@gmail.com>
2021-09-22 18:48:49 -0500

    Added headers to my ApolloClient.

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import Navbar from './components/Navbar';
 const client = new ApolloClient({
   uri: '/graphql',
   cache: new InMemoryCache(),
+  headers: {
+    authorization: localStorage.getItem('id_token') || '',
+  }
 });
 
 function App() {
