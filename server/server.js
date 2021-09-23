cb65be445832fe6e280536433337b0e449c93344
Ody <melodymonyok@gmail.com>
2021-09-23 12:45:17 -0500

    Added build connection.

--- server/server.js
+++ server/server.js
@@ -22,14 +22,6 @@ async function startServer() {
 };
 startServer();
 
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-//   context: authMiddleware,
-// });
-
-// server.applyMiddleware({ app });
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -38,9 +30,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
